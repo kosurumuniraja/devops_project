ed029070d7a7cffcb48dbc85365c40961d8df270
kosurumuniraja <86332515+kosurumuniraja@users.noreply.github.com>
2022-11-04 10:35:41 +0530

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,5 +2,5 @@
 readarray -t fontList < <(curl -u kosurumuniraja:9985259937@kK -k https://github.com/kosurumuniraja?tab=repositories | grep -w 'itemprop="name codeRepository"' | cut -d/ -f3 | cut -d'"' -f1)
 for i in "${fontList[@]}"; do
   echo "This is Repo name --> $i"
-  curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer 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" -k \https://api.github.com/repos/kosurumuniraja/$i/branches/master/protection
+  curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer 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" -k \https://api.github.com/repos/kosurumuniraja/$i/branches/master/protection
   done
